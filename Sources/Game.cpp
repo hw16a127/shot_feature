ed7dcfce8e08fc943de8654c9f04e5aa34684be2
hw16a127 <hw16a127@oecu.jp>
2017-11-28 15:55:22 +0900

    a, b, c終わり

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -1,9 +1,9 @@
 #include "Game.hpp"
 
 
-// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
-// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
+// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)@寺田
+// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)＠寺田
+// TODO: 砲台を青い壁に沿って上下に動かす。(C)＠寺田
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
@@ -16,16 +16,18 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
+bool botom;
 
 
 // ゲーム開始時に呼ばれる関数です。
 void Start()
 {
     cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
+    cannonPos = Vector2(-320, -150);
+    targetRect = Rect(260, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    botom = true;
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -54,6 +56,10 @@ void Update()
 
     // 雲の描画
     DrawImage("cloud1.png", cloudPos);
+    cloudPos.x += 3;
+    if (cloudPos.x >= 540) {
+        cloudPos.x = -530;
+    }
 
     // 弾の描画
     if (bulletPos.x > -999) {
@@ -63,6 +69,18 @@ void Update()
     // 砲台の描画
     FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
     DrawImage("cannon.png", cannonPos);
+    if (botom == true) {
+        cannonPos.y += 1;
+        if (cannonPos.y > -70) {
+            botom = false;
+        }
+    }
+    if (botom == false) {
+        cannonPos.y -= 1;
+        if (cannonPos.y < -145) {
+            botom = true;
+        }
+    }
 
     // ターゲットの描画
     FillRect(targetRect, Color::red);
